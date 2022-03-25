080e2d63594b95b369ba1b2b0d293df5fc9cb4c5
mac <mac@localhost>
2022-03-25 11:33:20 +0100

    First Commit for Complexe project

---
+++ Complexe-gi18-g2/src/aiac/gi18/java/complexe/Complexe.java
@@ -0,0 +1,52 @@
+package aiac.gi18.java.complexe;
+
+public class Complexe {
+
+	private double reel;
+	private double img;
+
+	public Complexe() {
+		this(0, 0);
+	}
+
+	public Complexe(double reel) {
+		this(reel, 0);
+	}
+
+	public Complexe(double reel, double img) {
+		this.reel = reel;
+		this.img = img;
+	}
+
+	public Complexe multiply(double scalar) {
+		Complexe ret = new Complexe();
+		ret.reel = this.reel * scalar;
+		ret.img = this.img * scalar;
+		return ret;
+		// return new Complexe(this.reel*scalar,this.img*scalar);
+	}
+
+	public Complexe add(Complexe c) {
+		return add(c.reel, c.img);
+	}
+
+	public Complexe add(double reel, double img) {
+		return new Complexe(this.reel + reel, this.img + img);
+	}
+
+	public Complexe sub(Complexe c) {
+		return add(-c.reel, -c.img);
+	}
+
+	public Complexe mul(Complexe c) {
+		Complexe ret = new Complexe();
+		ret.reel = this.reel * c.reel - this.img * c.img;
+		ret.img = this.reel * c.img + this.img * c.reel;
+		return ret;
+	}
+	
+	public String toString()
+	{
+		return this.reel+ " + "+this.img;
+	}
+}
---
+++ Complexe-gi18-g2/src/aiac/gi18/java/complexe/Main.java
@@ -0,0 +1,16 @@
+package aiac.gi18.java.complexe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Complexe c1,c2,c3;
+		c1=new Complexe(1, 2);
+		c2=new Complexe(10);
+		c3=new Complexe(-1, 4);
+		
+		System.out.println(c1);
+		System.out.println(c2);
+
+	}
+
+}
