cd097b118706c907e6076fd1f20fe8f2bc87c593
mac <mac@fe80:0:0:0:419:cfff:fe9c:c231%9>
2022-04-01 11:22:02 +0000

    Implementation de la methode somme

--- Complexe-gi18-g2/src/aiac/gi18/java/complexe/ListComplexe.java
+++ Complexe-gi18-g2/src/aiac/gi18/java/complexe/ListComplexe.java
@@ -9,5 +9,16 @@ public class ListComplexe extends ArrayList<Complexe> {
 		if(this.contains(e)) return false;
 		return super.add(e);
 	}
+	
+	public  Complexe somme() {
+		Complexe c=new Complexe();
+		
+		for (int i = 0; i < this.size(); i++) {
+			c=c.add(this.get(i));
+		}
+		
+		return c;
+		
+	}
 
 }
